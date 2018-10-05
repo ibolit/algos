f0b4e096d81d4319d505aa1c51171f7eafd24037
Timofey <dtv@me.com>
2018-10-05 08:27:04 +0300

    Did the stats thing. Doesn't seem to work.

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,11 +1,13 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats {
     private int size;
     private int trials;
     private int[] openSiteNumbers;
     private double mean;
     private double stddev;
-//    private double confidenceLo;
-//    private double confidenceHi;
     private double confidenceFactor;
 
 
@@ -17,7 +19,9 @@ public class PercolationStats {
         for(int i = 0; i < this.trials; i++) {
             Percolation percolation = new Percolation(size);
             while(!percolation.percolates()) {
-                percolation.open(1, 1);
+                int row = StdRandom.uniform(size - 1) + 1;
+                int col = StdRandom.uniform(size - 1) + 1;
+                percolation.open(row, col);
             }
             openSiteNumbers[i] = percolation.numberOfOpenSites();
         }
@@ -36,7 +40,7 @@ public class PercolationStats {
         return acc / trials;
     }
 
-    private double mean() {
+    public double mean() {
         return mean;
     }
 
@@ -50,7 +54,7 @@ public class PercolationStats {
         return Math.sqrt(acc / (trials - 1));
     }
 
-    private double stddev() {
+    public double stddev() {
         return stddev;
     }
 
@@ -59,7 +63,7 @@ public class PercolationStats {
     }
 
 
-    private double confidenceLo() {
+    public double confidenceLo() {
         return mean() - confidenceFactor;
     }
 
@@ -72,6 +76,19 @@ public class PercolationStats {
 
 
     public static void main(String[] args) {
+        if(args.length > 2) {
+            throw new RuntimeException("Too many arguments");
+        }
+        int[] size_n_trials = new int[2];
+        for(int i = 0; i < 2; i++) {
+            size_n_trials[i] = Integer.parseInt(args[i]);
+        }
+
+        PercolationStats ps = new PercolationStats(size_n_trials[0], size_n_trials[1]);
+        System.out.println(String.format("mean                    = %d", ps.mean()));
+        System.out.println(String.format("stddev                  = %d", ps.stddev()));
+        System.out.println(String.format("95% confidence interval = [%d, %d]", ps.confidenceLo(), ps.confidenceHi()));
+
         // test client (described below)
     }
 }
